759428304a50043bce034f178c1eac3ed9c6de76
Nick-DT-Flex\Nick <njcalapa@gmail.com>
2017-11-15 10:08:25 -0500

    Study of Knockout DataBinding, AJAX and C# MVC

---
+++ KnockoutStudy/Controllers/HomeController.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+using KnockoutStudy.Models;
+using KnockoutStudy.Model;
+
+namespace KnockoutStudy.Controllers
+{
+    public class HomeController : Controller
+    {
+        private KnockOutEntities koCxt = new KnockOutEntities();
+
+        public ActionResult Index()
+        {
+            ViewBag.Message = "Welcome to ASP.NET MVC!";
+
+            return View();
+        }
+
+        public JsonResult Save(Person person)
+        {
+            koCxt.ExecuteStoreCommand("DELETE Friend");
+            
+            foreach (Models.Friend frnd in person.Friends)
+            {                
+                //if (!IsAFriend(frnd.Name))
+                //{
+                    Model.Friend friend = new Model.Friend();
+                    friend.Name = frnd.Name;
+                    friend.IsOnTwitter = frnd.IsOnTwitter;
+                    friend.TwitterName = frnd.TwitterName;
+
+                    koCxt.Friend.AddObject(friend);
+                    koCxt.SaveChanges();
+            }
+
+            var message = string.Format("Saved {0} {1}", person.FirstName, person.LastName);
+            message += string.Format(" with {0} friends", person.Friends.Count);
+            message += string.Format(" ({0} on Twitter)", person.Friends.Where(x => x.IsOnTwitter).Count());
+
+            return Json(new { message });
+        }
+
+        public JsonResult GetFriendsList()
+        {
+            var frLinq = from c in koCxt.Friend
+                         select c;
+
+            return Json(frLinq);
+        }
+
+        public ActionResult About()
+        {
+            return View();
+        }
+    }
+}
---
+++ KnockoutStudy/obj/Debug/Package/PackageTmp/Scripts/KOStudy.js
@@ -0,0 +1,39 @@
+﻿
+function friend(name, twt, twtName) {
+    return {
+        name: ko.observable(name),
+        isOnTwitter: ko.observable(twt),
+        twitterName: ko.observable(twtName),
+        remove: function () {
+            viewModel.friends.remove(this);
+        }
+    };
+}
+
+function getList() {
+    var jsonResult = new Array();
+    var someResult = new Array();
+
+    var promise = testAjax();
+
+    promise.success(function (data) {
+        someResult = data;
+    });
+
+    for (var i = 0; i < someResult.length; i++) {
+        var fr = new friend(someResult[i].Name, someResult[i].IsOnTwitter, someResult[i].TwitterName);
+        jsonResult.push(fr);
+    }
+
+    return jsonResult; 
+}
+
+function testAjax() {
+    return $.ajax({
+        url: "/Home/GetFriendsList",
+        async: false, 
+        type: "post",
+        contentType: "application/json"
+    });
+}
+
